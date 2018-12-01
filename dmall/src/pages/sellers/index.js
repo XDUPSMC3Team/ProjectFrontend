6ae52f034239e923a39619dfdf3741e6bbf9ca28
ACERY1 <y841034081@gmail.com>
2018-12-01 23:24:38 +0800

    feat:引入Element + 构造项目结构

--- dmall/src/pages/sellers/index.js
+++ dmall/src/pages/sellers/index.js
@@ -1,5 +1,5 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './sellers.vue';
 import router from './router';
 
 Vue.config.productionTip = false;
--- dmall/vue.config.js
+++ dmall/vue.config.js
@@ -3,22 +3,22 @@ module.exports = {
   outputDir: 'dist',
   assetsDir: './assets', // 根据process.env 来控制是否走 CDN
   indexPath: '.html',
-  filenameHashing: true,
+  filenameHashing: true, // 加hash，防缓存
   pages: {
     customer: {
-      entry: 'src/customer.js',
+      entry: 'src/pages/customers/index.js',
       template: 'public/index.html',
       filename: 'customer.html',
       title: 'DMall 买家端',
     },
     seller: {
-      entry: 'src/customer.js',
+      entry: 'src/pages/sellers/index.js',
       template: 'public/index.html',
       filename: 'seller.html',
       title: 'DMall 卖家端',
     },
     admin: {
-      entry: 'src/admin.js',
+      entry: 'src/pages/admins/index.js',
       template: 'public/index.html',
       filename: 'admin.html',
       title: 'DMall 管理端',
@@ -26,6 +26,7 @@ module.exports = {
   },
   // chainWebpack: (wpConfig) => {}, // chainWebpack, 需要的时候开启
   devServer: {
-    port: 8999,
+    port: 8000,
+    proxy: '', // 用于开发时代理
   },
 };
