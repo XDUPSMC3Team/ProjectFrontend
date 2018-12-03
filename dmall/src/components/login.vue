964f1e22e1cb09e2a603e5e8cfdb563ea8839489
CWsuper <845202636@qq.com>
2018-12-03 13:26:33 +0800

    登录假组件，测试push

---
+++ dmall/src/components/login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="login">
+        <p>this is a view of login</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'login'
+}
+</script>
+
+<style lang="sass" scoped>
+
+</style>
+
+
