bc55de900aa6765b14eb3f4fd32600611c7c9897
ACERY1 <y841034081@gmail.com>
2018-12-06 19:05:10 +0800

    feat:增加Lab页

--- dmall/src/pages/customers/router.js
+++ dmall/src/pages/customers/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Home from './views/Home.vue';
+import Lab from './views/Lab.vue';
 
 Vue.use(Router);
 
@@ -10,6 +11,11 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home,
+    },
+    {
+      path: '/lab',
+      name: 'lab',
+      component: Lab,
     },
     {
       path: '/about',
