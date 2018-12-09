8ca0c373f4f1795ce456b4e5bbde7c1b893076eb
CWsuper <845202636@qq.com>
2018-12-09 21:57:52 +0800

    商家api调用成功

--- dmall/src/pages/admins/views/Search.vue
+++ dmall/src/pages/admins/views/Search.vue
@@ -1,31 +1,34 @@
 <script>
 import Shop from '../components/sellerInfo.vue';
-
+import {SearchShop} from '@/api/admin.js'
 export default {
   name: 'search',
   components: { Shop },
   props: {
-    search: {
-      type: String,
-      default: '111',
-    },
   },
   data() {
     return {
       shopInfo: {
-        name: '森马', // 店名
-        createTime: '2018-4-5', // 开店时间
-        description: '这是一家卖森马衣服的店铺，价格优惠，应有尽有', // 店描述
-        id: '1', // 店铺id
+        shop_name: 'sengMa', // 店名
+        create_time: '2018-4-5', // 开店时间
+        shop_desc: 'this is sengMa store!', // 店描述
+        seller_id: '1', // 店铺id
         status: '0', // 店铺状态
+        update_time: '2018-5-9',
       },
       showShop: false,
+      search: '',
     };
   },
   methods: {
-    searchShop() {
-      this.$successN('成功!', '搜索到了!');
-      this.showShop = true;
+    searchShop(shopId) {
+      SearchShop(shopId).then( (res) => {
+        if(res.code == 0){
+          this.shopInfo = res.data;
+          this.$successN('成功!', '搜索到了!');
+          this.showShop = true;
+        }
+      });
     },
   },
 };
@@ -34,11 +37,11 @@ export default {
 <template>
     <div class="search mt15 mb15 ml10 mr10">
         <el-input placeholder="请输入内容" v-model="search" class="search-input">
-            <el-button slot="append" icon="el-icon-search" @click="searchShop"></el-button>
+            <el-button slot="append" icon="el-icon-search" @click="searchShop(search)"></el-button>
         </el-input>
         <div class="search-result" v-if="showShop">
-            <Shop :name = shopInfo.name :createTime = shopInfo.createTime
-              :description = shopInfo.description :id = shopInfo.id :status= shopInfo.status />
+            <Shop :name = shopInfo.shop_name :createTime = shopInfo.create_time
+              :description = shopInfo.shop_desc :id = shopInfo.seller_id :status= shopInfo.status />
         </div>
     </div>
 </template>
